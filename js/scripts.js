e1a4681bb2007cb38882b644898441ebbca638c8
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-03-31 14:12:12 -0700

    adjust and remove a space that was in the second form, for alignment purposes

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
   $("form#transportation_survey").submit(function(event){
     event.preventDefault();
     $("#work-responses").show();
+  
     $("input:checkbox[name=work-transportation]:checked").each(function(){
       var workTransportationMode = $(this).val();
       $('#work-responses').append(workTransportationMode + "<br>");
